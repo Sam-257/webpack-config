93fe78d8df9341428c7ab697c58228a393ac8725
Sambhav Surana <Sambhav.Surana@bottomline.com>
2023-05-28 13:21:33 +0530

    Manually configuring webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import test from './images/test.jpg';
+import activeuser from './images/active-user.svg';
+import './styles/main.scss'
+
+class Game {
+  name = 'Violin Charades'
+}
+const myGame = new Game()
+// Create paragraph node
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.name}.`
+
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+// Append heading node to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
